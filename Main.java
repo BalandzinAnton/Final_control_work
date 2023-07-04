cca8354fd57df38036006346a79b58d0d394da1c
Антон Баландин <balandzin2602@MacBook-Pro-Anton.local>
2023-07-04 22:19:08 +0300

    Ãadd tasks

---
+++ Main.java
@@ -0,0 +1,10 @@
+package final_control_work.app;
+
+// Класс Main для запуска программы
+public class Main {
+    public static void main(String[] args) {
+        Database database = new Database();
+        Menu menu = new Menu(database);
+        menu.displayMenu();
+    }
+}
